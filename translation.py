dd8cffa12bc387b2a89c253a6486cf9ffc395ad7
X Roid <97869723+XRoiDX@users.noreply.github.com>
2022-03-20 13:18:08 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,27 +5,24 @@ class LuciferMoringstar(object):
     HELP_MSG = """**αdd tσ чσur grσup αѕ αdmín αnd í prσvídє mσvíєѕ**"""
 
     ABOUT_MSG = """
-🤖 ᴍy ɴᴀᴍᴇ [{bot_name}](t.me/{bot_username})
-
-🧑 ᴍy ᴅᴇᴠ : [ᵃˡᶠʳᵉᵈ ᵐᵃʳᵃˢʰᵉˡ](t.me/Space4AFML)
-
-🧑 ᴍy ᴄʀᴇᴀᴛᴏʀ : {dev_name}
-
-📦 ᴍy ꜱᴏᴜʀᴄᴇ : [¢ℓι¢к нєяє](https://t.me/Team_Venom_Bot/3)
-
-📺 ᴍy yᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ : [¢ℓι¢к нєяє](https://tinyurl.com/y6wlkv8g)
-
-🗃️ ᴍy ʟɪʙʀᴀʀy : [ρуяσgяαм](https://docs.pyrogram.org/)
-
-🖇️ ᴍy ʟᴀɴɢᴜᴀɢᴇ : [ρутнσи 3](https://www.python.org/)
-
-🗃️ ᴍy ᴅᴀᴛᴀʙᴀꜱᴇ : [мσиgσ∂в](https://www.mongodb.com/)
-
-🗳️ ᴍy ꜱᴇʀᴠᴇʀ : [нєяσкυ](https://www.heroku.com/)
-
-🏷️ ᴍy ᴜᴩᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ : [χяσι∂ вσтz](t.me/XRoiD_BotZ)
-
-🗣️ ᴍy ꜱᴜᴩᴩᴏʀᴛ ɢʀᴏᴜᴩ : [χяσι∂ ѕυρρσят](t.me/XRoid_Support)"""
+<b>🔥𝓗𝓲  𝓽𝓱𝓮𝓻𝓮 </b>{}
+    
+✨ <b>My Name :</b> 𝗕𝗟𝗔𝗖𝗞 𝗣𝗔𝗡𝗧𝗛𝗘𝗥
+👨‍💻 <b>Developers :</b> 𝙰𝚕𝚏𝚛𝚎𝚍 𝙼𝚊𝚛𝚜𝚑𝚎𝚕 [𝙾𝙵𝙵𝙻𝙸𝙽𝙴] & 𝙲𝚒𝚕𝚕𝚊𝚗 𝙼𝚞𝚛𝚏𝚢 [𝚃𝙶]
+🍀 <b>Data Base :</b> Mango DB
+📝 <b>Language :</b> Python3
+🧰 <b>Framework :</b> Pyrogram
+📡 <b>Server :</b> Heroku
+🌹 <b>Build Status :</b> 𝚅2.5
+🤖𝕭𝖔𝖙 𝕱𝖊𝖆𝖙𝖚𝖗𝖊𝖘
+💥<b>IMDB Supported</b>
+💥<b>Inline Search</b>
+💥<b>Auto Filter</b>
+💥<b>Allow Groups</b>
+💥<b>Ids & User Info</b>
+💥<b>Ban , Unban Users</b>
+⚙️𝗣𝗼𝘄𝗲𝗿𝗱 𝗕𝘆 :- 𝙼𝙻𝙼 𝙽𝙴𝚆 𝙼𝙾𝚅𝙸𝙴𝚂
+"""
 
     FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
 
