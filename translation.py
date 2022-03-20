7c9af938a12a4c1fd18530bdd1dc482f0720397b
X Roid <97869723+XRoiDX@users.noreply.github.com>
2022-03-20 13:27:01 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -42,16 +42,17 @@ Hello 👋〘 {mention} 〙,
 
 Couldn't Find {query}?  Please Click Your Request Movie Name"""
     GET_MOVIE_1 = """
-📽️ Requested Movie : [{query}]({url})
-👤 Requested By : {mention}
-🎬 Title : [{title}]({url})
-📆 Year : {year}
-🌟 Rating {rating}/10
-🗨️ Genres {genres}"""
+📽️ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : [{query}]({url})
+👤 ʀᴇqᴜᴇꜱᴛᴇᴅ ʙy : {mention}
+🎬 ᴛɪᴛʟᴇ : [{title}]({url})
+📆 yᴇᴀʀ : {year}
+🌟 ʀᴀᴛɪɴɢ {rating}/10
+🗨️ ɢᴇɴᴇʀꜱ {genres}
+🎥 ᴍʟᴍ ɴᴇᴡ ᴍᴏᴠɪᴇꜱ"""
 
 
     GET_MOVIE_2 = """
-📽️ Requested Movie : {query}
-👤 Requested By : {mention}
+📽️ ʀᴇqᴜᴇꜱᴛᴇᴅ ᴍᴏᴠɪᴇ : {query}
+👤 ʀᴇqᴜᴇꜱᴛᴇᴅ ʙy : {mention}
 
 © **{chat}**"""
